a8bcbac2ea825cef312c09bc72ad36ade5af08aa
Tigran Aivazian <tigran@bibles.org.uk>
2012-09-11 13:35:39 +0100

    Don't bind-mount system fonts.

--- launchpad/kpdfdjview.sh
+++ launchpad/kpdfdjview.sh
@@ -7,11 +7,6 @@ echo unlock > /proc/fiveway
 # we're always starting from our working directory
 cd /mnt/us/kpdfdjview/
 
-# bind-mount system fonts
-if ! grep /mnt/us/kpdfdjview/fonts/host /proc/mounts; then
-	mount -o bind /usr/java/lib/fonts /mnt/us/kpdfdjview/fonts/host
-fi
-
 # check if we are supposed to shut down the Amazon framework
 if test "$1" == "--framework_stop"; then
 	shift 1
@@ -23,11 +18,6 @@ killall -stop cvm
 
 # finally call reader
 ./reader.lua "$1" 2> /mnt/us/kpdfdjview/crash.log || cat /mnt/us/kpdfdjview/crash.log
-
-# unmount system fonts
-if grep /mnt/us/kpdfdjview/fonts/host /proc/mounts; then
-	umount /mnt/us/kpdfdjview/fonts/host
-fi
 
 # always try to continue cvm
 killall -cont cvm || /etc/init.d/framework start
