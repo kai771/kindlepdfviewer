df7a93e98a0bf01f567425939c72eee2ba67265e
Tigran Aivazian <tigran@bibles.org.uk>
2012-09-17 20:25:04 +0100

    Save the debug messages in the crash.log.

--- launchpad/kpdfdjview.sh
+++ launchpad/kpdfdjview.sh
@@ -17,7 +17,7 @@ fi
 killall -stop cvm
 
 # finally call reader
-./reader.lua "$1" 2> /mnt/us/kpdfdjview/crash.log || cat /mnt/us/kpdfdjview/crash.log
+./reader.lua "$1" > /mnt/us/kpdfdjview/crash.log 2>&1 || cat /mnt/us/kpdfdjview/crash.log
 
 # always try to continue cvm
 killall -cont cvm || /etc/init.d/framework start
